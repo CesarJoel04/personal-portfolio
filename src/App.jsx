8e6f3ca1dd2aae7fe3e00f0b2877183e2f381233
Cesar <cesarestevez04@gmail.com>
2025-11-22 11:48:38 -0600

    config github deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import './App.css'
 
 function App() {
-  return (
-    <div className="container text-center mt-5">
-      <h1 className="text-primary">Bootstrap is Working!</h1>
-      <button className="btn btn-success">Click Me</button>
-    </div>
-  );
+ 
 }
 
 export default App
